efbae1258856dc108fbbea354ec7ce9ae7ab8c81
Ralf <ralflopez3@gmail.com>
2022-10-05 23:31:28 +0800

    init

--- src/main/java/com/example/resteasyimageupload/HelloResource.java
+++ src/main/java/com/example/resteasyimageupload/HelloResource.java
@@ -1,14 +1,61 @@
 package com.example.resteasyimageupload;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 @Path("/hello-world")
 public class HelloResource {
-    @GET
+    @POST
     @Produces("text/plain")
-    public String hello() {
+    @Consumes("multipart/form-data")
+    public String hello(MultipartFormDataInput input) {
+        Map<String, List<InputPart>> formParts = input.getFormDataMap();
+
+        List<InputPart> inPart = formParts.get("file");
+
+        for (InputPart inputPart : inPart) {
+
+            try {
+
+                // Retrieve headers, read the Content-Disposition header to obtain the original name of the file
+                MultivaluedMap<String, String> headers = inputPart.getHeaders();
+                System.out.println(parseFileName(headers));
+
+                // Handle the body of that part with an InputStream
+                InputStream istream = inputPart.getBody(InputStream.class,null);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
         return "Hello, World!";
+    }
+
+    // Parse Content-Disposition header to get the original file name
+    private String parseFileName(MultivaluedMap<String, String> headers) {
+
+        String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
+
+        for (String name : contentDispositionHeader) {
+
+            if ((name.trim().startsWith("filename"))) {
+
+                String[] tmp = name.split("=");
+
+                String fileName = tmp[1].trim().replaceAll("\"","");
+
+                return fileName;
+            }
+        }
+        return "randomName";
     }
 }
